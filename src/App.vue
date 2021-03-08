47f5af87df6bea1747251494ac523c1ba48de95d
Markus <m.kraijenoord@gmail.com>
2021-03-08 14:15:14 +0100

    Add translation data

--- src/App.vue
+++ src/App.vue
@@ -130,11 +130,13 @@
             <br>
             From large scale predictive maintenance to precision agriculture,
             or from perpetual weather stations to inventory management.
-            ZED specializes in customizing solutions based on the clients needs and aids in a seamless integration into existing systems.
+            ZED specializes in customizing solutions based on the clients needs and aids in a seamless integration into
+            existing systems.
             <br>
             <br>
             By harvesting the ambient energy from its surroundings ZED’s solutions don't require batteries,
-            greatly reducing both the material and maintenance costs. This reduction in costs enables new large scale applications not seen before.
+            greatly reducing both the material and maintenance costs. This reduction in costs enables new large scale
+            applications not seen before.
           </v-responsive>
         </v-container>
 
@@ -466,123 +468,76 @@
 </template>
 
 <script>
+  import _ from 'lodash'
+
   export default {
     name: 'App',
     components: {},
     created () {
       this.setFavicon(
-          window.matchMedia('(prefers-color-scheme: dark)').matches
+        window.matchMedia('(prefers-color-scheme: dark)').matches
       )
     },
     methods: {
-      setFavicon(darkMode = false) {
+      setFavicon (darkMode = false) {
         const favicon = darkMode ? 'favicon_white.ico' : 'favicon_black.ico'
         document.getElementById('favicon').setAttribute('href', favicon)
+      }
+    },
+    computed: {
+      menu () {
+        return this.$t('menu', { returnObjects: true })
+      },
+      articles () {
+        return _.merge(
+          [
+            {
+              src: require('./assets/ZED_FYI_Team.jpg'),
+              link: 'https://www.airbus.com/newsroom/press-releases/en/2019/06/pioneering-student-team-from-the-netherlands-wins-airbus-fly-your-ideas-2019-global-competition.html'
+            },
+            {
+              src: require('./assets/ZED_Impact_competition.png'),
+              link: 'https://4tuimpactchallenge.nl/news/384'
+            },
+            {
+              src: require('./assets/ZED_Delta_Team.jpg'),
+              link: 'https://www.delta.tudelft.nl/article/why-smart-electronics-are-becoming-battery-free'
+            }
+          ],
+          this.$t('articles', { returnObjects: true })
+        )
+      },
+      features () {
+        return _.merge(
+          [
+            { image: require('./assets/ZED_icon_White_Maintenance.png') },
+            { image: require('./assets/ZED_icon_White_Transportation.png') },
+            { image: require('./assets/ZED_icon_White_Inventory.png') },
+            { image: require('./assets/ZED_icon_White_Detection.png') },
+            { image: require('./assets/ZED_icon_White_Agriculture.png') },
+            { image: require('./assets/ZED_icon_White_weatherstations.png') }
+          ],
+          this.$t('features', { returnObjects: true })
+        )
+      },
+      team () {
+        return _.merge(
+          [
+            { image: require('./assets/VP.jpg') },
+            { image: require('./assets/Niels.jpg') },
+            { image: require('./assets/John.jpg') },
+            { image: require('./assets/Suryansh.jpg') },
+            { image: require('./assets/Sujay.jpg') },
+            { image: require('./assets/Josine.jpg') }
+          ],
+          this.$t('team', { returnObjects: true })
+        )
       }
     },
     data () {
       return {
         drawer: false,
-        email: 'info@ZED-IoT.com',
-        menu: [
-          { text: 'About us', link: '#about-us' },
-          { text: 'Features', link: '#features' },
-          { text: 'The team', link: '#team' },
-          { text: 'News', link: '#news' },
-          { text: 'Contact', link: '#contact' }
-        ],
-        articles: [
-          {
-            src: require('./assets/ZED_FYI_Team.jpg'),
-            title: 'Winning the Airbus FYI competition',
-            text: 'Under the name of Zero Heroes, ZED has won the prestigious Airbus Fly Your Ideas price. From 300 steams ZED convinced the jury with their energy harvesting solutions inside aircrafts.',
-            link: 'https://www.airbus.com/newsroom/press-releases/en/2019/06/pioneering-student-team-from-the-netherlands-wins-airbus-fly-your-ideas-2019-global-competition.html'
-          },
-          {
-            src: require('./assets/ZED_Impact_competition.png'),
-            title: 'Winning the 4TU Impact challenge',
-            text: 'After wining the TU-Delft impact competition, ZED wins the national 4TU Impact challenge. Winning tickets to the World Expo in Dubai, where ZED will join the Dutch trade delegation representing the Netherlands.',
-            link: 'https://4tuimpactchallenge.nl/news/384'
-          },
-          {
-            src: require('./assets/ZED_Delta_Team.jpg'),
-            title: 'Delta, TuDelft article',
-            text: 'Read why smart electronics are becoming battery free.',
-            link: 'https://www.delta.tudelft.nl/article/why-smart-electronics-are-becoming-battery-free'
-          }
-        ],
-        features: [
-          {
-            image: require('./assets/ZED_icon_White_Maintenance.png'),
-            title: 'Large scale Predictive Maintenance',
-            text: 'Masses of sensors spread across large areas detecting abnormalities reducing down time of systems,' +
-                'only becomes feasible with low cost and low maintenance i.e. batteryless and wireless sensors.'
-          },
-          {
-            image: require('./assets/ZED_icon_White_Transportation.png'),
-            title: 'Smart Transportation',
-            text: 'Scalable energy harvesting sensors offer a cheap time-efficient way of controlling logistics.' +
-                'By for example measuring the seat occupancy, or detecting whether freight is correctly secured.'
-          },
-          {
-            image: require('./assets/ZED_icon_White_Inventory.png'),
-            title: 'Inventory Management',
-            text: 'Zero energy devices are able to detect the volumes of inventory that is held at the warehouses.' +
-                'The batteryless sensors measure when inventory is low and wirelessly transmitted to a management system.'
-          },
-          {
-            image: require('./assets/ZED_icon_White_Detection.png'),
-            title: 'Flood Detection',
-            text: 'By applying large numbers of zero energy devices into dikes, they can warn when the tides become too high.' +
-                'Without the need for wires which must be dug into the ground weakening the dikes.'
-          },
-          {
-            image: require('./assets/ZED_icon_White_Agriculture.png'),
-            title: 'Precision Agriculture',
-            text: 'Agriculture is becoming high tech, helping optimising yields.' +
-                'With the limitation of not being able to put wires into the ground,' +
-                'ZED’s wireless and batteryless device can be used to measure crop,' +
-                'soil and air quality throughout large fields.'
-          },
-          {
-            image: require('./assets/ZED_icon_White_weatherstations.png'),
-            title: 'Perpetual Weather Stations',
-            text: 'With self powered weather stations from ZED throughout the city,' +
-                'the much needed fine-grained data can be collected, improving weather predictions. '
-          }
-        ],
-        team: [
-          {
-            image: require('./assets/VP.jpg'),
-            name: 'Dr. R.R. Venkatesha Prasad',
-            text: 'Known fondly in the team as VP, Dr. Prasad is an associate professor at the Embedded and Networked Systems Group at TU, Delft. He spends three quarters of his time teaching and researching at the university and the other quarter working for ZED using his experience and network within the industry. He possesses 6 years of startup experience. In his startup venture, Esqube Communication Solutions, he developed an integrated VoIP--Telephony--web application suite. One real-time application he developed, to bridge VoIP calls anonymously, called Click-to-Talk, was appreciated by Ebay.com. The startup filed for eight patent applications and three PCT applications and was selected as top 100 IT innovators in India (NASSCOM 2006) and top 100 promising companies in Asia (RedHerring 2008). VP has more than 20 years of network, wireless, and IoT research expertise having mentored many PhDs and master students thus far. A never ending pool of ideas and enthusiasm, VP ensures that ZED never misses an opportunity either technical or financial.'
-          },
-          {
-            image: require('./assets/Niels.jpg'),
-            name: 'Ir. N.H. Hokke',
-            text: 'Niels, a recent graduate supervised by VP, worked on the topic of batteryless wireless communication in his masters at TU Delft. Instead of starting as a PhD and adding more research papers into an already full archive, Niels instead believes in transforming state of the art research into real life tangible products for the benefit of society. As CEO, Niels is fully focusing all of his time on building up ZED as a vehicle which can move our innovation from the labs of the university onto the industrial factory floor where it can truly make a difference saving time and money for our clients.'
-          },
-          {
-            image: require('./assets/John.jpg'),
-            name: 'Dr. J.E.J. Schmitz',
-            text: 'Dr. John Schmitz recently retired as the dean of the faculty of Electrical Engineering, Mathematics and Computer Science (EEMCS or EWI in Dutch) at the Technical University of Delft. Before this, John was SVP and Chief Intellectual Property Officer of NXP Semiconductors. He is attributed as the co-founder of NXP Semiconductors and has immense technical, scientific and commercial experience. He is advising ZED as honorary president on both technical and commercial aspects where he directs, mentors and guides the team to becoming a successful startup.  '
-          },
-          {
-            image: require('./assets/Suryansh.jpg'),
-            name: 'Ir. S. Sharma',
-            text: 'Suryansh, who also recently graduated under VP on the topic of batteryless wireless sensing, is currently pursuing a PhD at TU Delft on AIoT where he combines the power of AI and IoT systems to extreme applications. Suryansh kickstarted ZED’s journey at the Impact Challenge, granting the team a platform for experiencing entrepreneurship and gaining much needed structure and direction. Passionate for Innovation, He believes in the power of ideas and wants to transform research papers into tangible technology which can benefit the society at large.'
-          },
-          {
-            image: require('./assets/Sujay.jpg'),
-            name: 'Dr. S. Narayana',
-            text: 'Sujay, is currently working as a postdoc with VP at TU Delft. Having finished his Phd in resource constrained space IoT devices, Sujay is an expert in coordinating operations and guiding technical development within the team. Extremely methodical and scientifically sound, he brings a wide range of industrial engineering as well as research experience to ZED. With him on board, we look at “space” as another frontier for us to conquer.'
-          },
-          {
-            image: require('./assets/Josine.jpg'),
-            name: 'J. I. van der Velde',
-            text: 'Business associate Josine is a student at NHL-Stenden studying International Business and Management Studies. She is doing her internship full-time at ZED and helps refine the business structure of ZED. Instrumental in developing a marketing strategy and reaching out to potential clients, Josine is always up for new challenges. While working she likes to keep control over the overall scope while ensuring a fun motivated atmosphere within the team.'
-          }
-        ],
+        email: this.$t('email'),
         stats: []
       }
     }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   vuetify,
   render: h => h(App),
   mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
