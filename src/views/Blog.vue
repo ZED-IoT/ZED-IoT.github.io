37e64ee1e0b922663829276a210898bb6604abf6
Markus <m.kraijenoord@gmail.com>
2021-07-11 22:16:33 +0200

    Add blog translations

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,11 +1,25 @@
 <template>
   <v-main>
     <div class="about">
-      <h1>This is an about page {{ $route.params.blog }}</h1>
+      <h1>{{ blog }}</h1>
     </div>
   </v-main>
 </template>
 
 <script>
-
+  export default {
+    name: 'Blog',
+    methods: {
+      getTranslationObject () {
+        const key = this.$route.params.blog
+        const translation = this.$t(`blog:${key}`, { returnObjects: true })
+        return translation !== key ? translation : []
+      }
+    },
+    computed: {
+      blog () {
+        return this.getTranslationObject()
+      }
+    }
+  }
 </script>
