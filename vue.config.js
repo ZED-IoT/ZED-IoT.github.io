4a2b50fccdfb2f66a0cf3fbfcb49f3030d435ae1
Unknown <davidzwa@gmail.com>
2022-04-08 17:06:41 +0200

    Remove prerender SPA plugin config

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,5 @@ module.exports = {
   outputDir: 'dist',
   'transpileDependencies': [
     'vuetify'
-  ],
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/'
-      ],
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true,
-      postProcess (context) {
-        context.html = context.html.replace('http://localhost:8000', 'https://www.zed-iot.com')
-        return context
-      }
-    }
-  }
+  ]
 }
